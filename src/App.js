e19ae49d9bb6b8516d757c1b1278cc24d9134b3a
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2023-09-22 01:30:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [xAxis, setxAxis] = useState(0);
+    const [yAxis, setyAxis] = useState(0);
+
+    return (
+
+        <div className = 'main_container'>
+        <button className = 'horizontal'
+        onClick = {
+            () => setyAxis(yAxis - 50)
+        }
+        disabled = {
+            yAxis === 0
+        }>Up</button>
+        <div className = 'container'>
+        <button className = 'vertical'
+        onClick = {
+            () => setxAxis(xAxis - 50)
+        }
+        disabled = {
+            xAxis === 0
+        }>Left</button>
+        <div className = 'main'>
+        
+        <div className = 'box'
+        style = {
+            {
+                left: xAxis,
+                top: yAxis
+            }
+        } > </div> 
+        </div>
+        <button className = 'vertical'
+        onClick = {
+            () => setxAxis(xAxis + 50)
+        }
+        disabled = {
+            xAxis === 450
+        }> Right </button>
+        </div> 
+
+       <button className = 'horizontal'
+        onClick = {
+            () => setyAxis(yAxis + 50)
+        }
+        disabled = {
+            yAxis === 450
+        }> Bottom </button> 
+        </div>
+    );
 }
 
 export default App;
